ef2ded5a3a4fa9b4e8f4f7e8d5c0f7f35ba46f72
Antony Rafael <antonyrafaelsouzaa@gmail.com>
2024-12-21 16:32:13 -0300

    Salvando Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -45,6 +45,14 @@ public class Utils {
 			return cell;
 		});
 	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
 		tableColumn.setCellFactory(column -> {
